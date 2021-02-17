e4d270a2c22e79cfabf7c953deb46216c0232217
AugustoCiuffoletti <augusto@di.unipi.it>
2021-02-17 16:13:18 +0100

    nuova modifica

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 import './style.css';
 
 document.getElementById("demo").innerHTML = "Primo<br>";
-document.getElementById("demo").innerHTML += "Secondo<br>";
+document.getElementById("demo").innerHTML += "nuova riga<br>";
 document.getElementById("demo").innerHTML += "<b>Terzo</b>";
